8427f8aceb051e046de84c0d1f9b9fbf6dd43da0
eitankrajewski <122746971+eitankrajewski@users.noreply.github.com>
2023-03-27 21:37:37 -0400

    Update VinylTracker.java

--- VinylTracker.java
+++ VinylTracker.java
@@ -12,30 +12,52 @@ public class VinylTracker {
         String vinyls = scanner.nextLine().toUpperCase();
         ArrayList<String> vinylList = new ArrayList<>();
 
-        switch(vinyls) {
-            case "A" -> {
+        while (vinyls != "Q") {
+            switch (vinyls) {
+                case "A" -> {
                     System.out.println("Which vinyls would you like to add?: ");
                     vinyls = scanner.nextLine();
                     vinylList.add(vinyls);
-                    System.out.println(vinylList);
-            }
-            case "R" -> vinylList.remove(vinyls);
-            case "Q" -> {
-                return;
-            }
-            default -> {
-                vinylList.add(vinyls);
+                    System.out.println("Ok. " + vinyls + " added to the list.");
+                    printOptions();
+                    vinyls = scanner.nextLine();
+                    if(vinyls == "P") {
+                        System.out.println(vinylList);;
+                    }
+                }
+                case "R" -> {
+                    System.out.println("Which vinyls would you like to remove?: ");
+                    vinyls = scanner.nextLine();
+                    vinylList.remove(vinyls);
+                    System.out.println("Ok. " + vinyls + " removed from the list.");
+                    printOptions();
+                    vinyls = scanner.nextLine();
+                    if(vinyls == "P") {
+                        System.out.println(vinylList);;
+                    }
+                }
+                case "Q" -> {
+                    return;
+                }
+                default -> {
+                    System.out.println("Invalid choice selection. Please choose one of the choices from the list.");
+                    printOptions();
+                    vinyls = scanner.nextLine();
+                }
             }
         }
-
     }
+
 
     private static void printOptions() {
         System.out.println("""
             What would you like to do?
             1) Add vinyls
             2) Remove vinyls
-            3) Quit
+            3) Print out vinyl list
+            4) Quit
             Enter option: """);
     }
+    
 }
+
